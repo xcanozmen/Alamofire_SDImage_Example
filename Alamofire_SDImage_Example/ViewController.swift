14d77520376683a7ea12acba02ca6380a4dca80e
xcanozmen <can35ozmen@gmail.com>
2021-03-04 12:09:28 +0300

    Initial Commit

---
+++ Alamofire_SDImage_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamofire_SDImage_Example
+//
+//  Created by Can on 4.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
